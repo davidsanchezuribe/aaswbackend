8847989bcb8b47a75d150de7a8c55eaf05e6711a
dei <dei@deis-iMac.local>
2022-10-27 17:40:05 -0500

    API orders

--- src/server/pedidoAPI.ts
+++ src/server/pedidoAPI.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 // import { body, validationResult } from 'express-validator';
 import { pedidoRepository } from '../AppDataSource';
 // import Pedido from '../model/Pedido';
@@ -14,8 +15,39 @@ pedidoAPI.get(
   '/',
   async (req: Request, res: Response) => {
     try {
-      const allOrders = await pedidoRepository.find();
+      const allOrders = await pedidoRepository.find({
+        relations: {
+          cliente: true,
+          entradas: {
+            producto: true,
+          },
+        },
+      });
       return res.status(200).json({ orders: allOrders });
+    } catch (error) {
+      return res.status(400).json({ msg: getErrorMessage(error) });
+    }
+  },
+);
+
+pedidoAPI.post(
+  '/incrementarimpresion',
+  body('id').isInt(),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { id } = req.body;
+    const orderToUpdate = await pedidoRepository.findOneBy({ id });
+    try {
+      if (orderToUpdate != null) {
+        orderToUpdate.numero_impresiones += 1;
+        await pedidoRepository.save(orderToUpdate);
+      } else {
+        return res.status(400).json({ msg: `El pedido ${id} no existe` });
+      }
+      return res.status(200).json({ orderToUpdate });
     } catch (error) {
       return res.status(400).json({ msg: getErrorMessage(error) });
     }
